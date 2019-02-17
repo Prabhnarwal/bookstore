bef9bc052fba6a2617dee42064576b0d4d70b549
1894830 <1894830@E21035.CEGEPGIM.LCL>
2019-02-17 11:10:13 -0500

    orderdetail

--- bookstore1/src/orderdetail.java
+++ bookstore1/src/orderdetail.java
@@ -1,17 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package bookstore1;
 
 /**
  *
  * @author 1894830
  */
 public class orderdetail {
-    
-     String quantity;
+    String quantity;
     
     void setquantity(String Quantity)
     {

d8a0171d91865f1f87aaa9529bd1f85afccd446e
1894994 <1894994@E21058.CEGEPGIM.LCL>
2019-02-17 10:19:23 -0500

    book

---
+++ bookstore1/src/bookstore1/book.java
@@ -0,0 +1,53 @@
+package bookstore1;
+
+public class book {
+
+    String title;
+    Double price;
+    int year;
+    long ISBN;
+
+    void settitle(String t) {
+        t = title;
+    }
+
+    void setprice(Double pr) {
+        pr = price;
+    }
+
+    void setyear(int y) {
+        y = year;
+    }
+
+    void setISBN(long isbn) {
+        isbn = ISBN;
+    }
+
+    String gettitle() {
+        return title;
+    }
+
+    Double getprice() {
+        return price;
+
+    }
+
+    int getyear() {
+        return year;
+    }
+
+    long getISBN() {
+        return ISBN;
+    }
+
+    void Display(String t, Double pr, int y, long isbn) {
+        System.out.println(t + " " + pr + " " + y + " " + " " + isbn);
+    }
+
+    public book(String t, Double pr, int y, long isbn) {
+        t = title;
+        pr = price;
+        y = year;
+        isbn = ISBN;
+    }
+}

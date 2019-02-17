bb998a3ebbc96f148526a78ea99d814a16f1932e
1894828 <1894828@E21057.CEGEPGIM.LCL>
2019-02-17 10:13:25 -0500

    customer

---
+++ bookstore1/src/bookstore1/customer.java
@@ -0,0 +1,36 @@
+package bookstore1;
+
+import java.util.Date;
+
+public class customer {
+
+    int cID;
+    Date registerDate;
+
+    void setcID(int cusID) {
+        cusID = cID;
+    }
+
+    void setregisterDate(Date regDate) {
+        regDate = registerDate;
+    }
+
+    public customer(int cusID, Date regDate) {
+
+        cID = cusID;
+        registerDate = regDate;
+    }
+
+    int getcID() {
+        return cID;
+    }
+
+    Date getregisterDate() {
+        return registerDate;
+    }
+
+    void Display(int cusID, Date regDate) {
+        System.out.println(cusID + " " + regDate);
+    }
+
+}

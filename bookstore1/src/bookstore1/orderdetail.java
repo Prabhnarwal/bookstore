d442274303183de865ba2ad2bc165213d0722411
1894830 <1894830@E21035.CEGEPGIM.LCL>
2019-02-17 11:14:30 -0500

    orderdetail

--- bookstore1/src/bookstore1/orderdetail.java
+++ bookstore1/src/bookstore1/orderdetail.java
@@ -1,31 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package bookstore1;
 
-/**
- *
- * @author 1894830
- */
 public class orderdetail {
-    
-     String quantity;
-    
-    void setquantity(String Quantity)
-    {
-        Quantity=quantity;
+
+    String quantity;
+
+    void setquantity(String Quantity) {
+        Quantity = quantity;
     }
-     String getQuantity() {
+
+    String getQuantity() {
         return quantity;
-}
-     public orderdetail(String 
-             Quantity)
-     {
-         quantity=Quantity;
-     }
-     void diaplay4(String Quantity){
-         System.out.println(""+Quantity);
-     }
+    }
+
+    public orderdetail(String Quantity) {
+        quantity = Quantity;
+    }
+
+    void Display5(String Quantity) {
+        System.out.println(Quantity);
+    }
 }

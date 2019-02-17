fdb368226b3bb968095886c8c5706c8d23ad0df5
1894994 <1894994@E21058.CEGEPGIM.LCL>
2019-02-17 10:19:23 -0500

    initial

--- bookstore1/src/bookstore1/Bookstore1.java
+++ bookstore1/src/bookstore1/Bookstore1.java
@@ -1,21 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package bookstore1;
 
-/**
- *
- * @author 1894994
- */
 public class Bookstore1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
+        book b=new book("Puzzles",12.50,2018,1098989834);
+        b.settitle("Hello");
+        b.setprice(13.25);
+        b.setyear(2019);
+        b.setISBN(45679997);
+        
+        System.out.println(b.gettitle() + " " + b.getprice() + " " + b.getyear()+" "+b.getISBN());
+        b.Display("abc",24.50,2017,9090909);
     }
     
 }

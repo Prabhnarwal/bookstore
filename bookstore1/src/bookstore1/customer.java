3158dd7f2928846fc31d81216be52e087d7fca78
Taran <temp@E21058.CEGEPGIM.LCL>
2019-03-03 08:48:25 -0500

    customer

--- bookstore1/src/bookstore1/customer.java
+++ bookstore1/src/bookstore1/customer.java
@@ -1,5 +1,6 @@
 package bookstore1;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class customer extends personClass {
@@ -31,5 +32,49 @@ public class customer extends personClass {
     void Display2(int cusID, Date regDate) {
         System.out.println(cusID + " " + regDate);
     }
-
+public static String createId(Date newDate, String Fname)
+    {
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+        String date=sdf.format(newDate);
+        String[] dt=date.split("-");
+        String dtInt;
+        if(Integer.parseInt(dt[2])%2==0)
+        {
+            dtInt="E";
+           
+        }
+        else
+        {
+             dtInt="O";
+        }
+        String mnt=dt[1];
+        String yr=dt[0].substring(2);
+        String fchar="";
+        if(Fname.length()==1)
+            fchar+="x";
+        if(Fname.length()==2)
+            fchar+=Fname;
+        else if (Fname.equals(""))
+            fchar="XX";
+        else 
+            fchar=Fname.substring(0,1);
+        
+        String lchar="";
+        if(Fname.length()==1)
+            lchar+="x";
+        if(Fname.length()==2)
+            lchar+=Fname;
+        else if (Fname.equals(""))
+            lchar="XX";
+        else 
+            lchar=Fname.substring(Fname.length()-1);
+        String id=yr+"-"+mnt+"-"+dtInt+"-";
+        if (Fname.equals(""))
+            id+="XX";
+        else if (Fname.length()==2)
+            id+=Fname;
+        else 
+            id+=fchar.toUpperCase()+lchar.toUpperCase();
+         return id;
+    }
 }

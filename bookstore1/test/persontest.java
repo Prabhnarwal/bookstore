8c8c1cfaebf7ed9ffa9dba173dd232df96179f1b
1894830 <1894830@E21055.CEGEPGIM.LCL>
2019-03-03 09:22:09 -0500

    person

---
+++ bookstore1/test/persontest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Date;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 1894830
+ */
+public class persontest {
+    
+    public persontest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+@Test
+    public void createIDTest() {
+        System.out.println("Test for personclass");
+        Date newDate = new Date();
+        String fName = "Prabhjot";
+        String expectedResult = "19-03-O-PT";
+        String realResult = bookstore1.customer.createId(newDate,fName);
+        assertEquals(expectedResult, realResult);}
+}
+
+ 
+

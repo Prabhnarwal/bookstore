d9682b250927fbb344aa50a0f0fbf6a7998ab212
1894828 <1894828@E21057.CEGEPGIM.LCL>
2019-03-03 09:26:44 -0500

    author

--- bookstore1/src/bookstore1/author.java
+++ bookstore1/src/bookstore1/author.java
@@ -5,6 +5,9 @@
  */
 package bookstore1;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  *
  * @author 1894830
@@ -40,5 +43,50 @@ public class author extends personClass {
     void Display4(String BookName, String Authorname) {
         System.out.println(BookName + ""
                 + Authorname);
+    }
+    public static String createId(Date newDate, String bookName)
+    {
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+        String date=sdf.format(newDate);
+        String[] dt=date.split("-");
+        String dtInt;
+        if(Integer.parseInt(dt[2])%2==0)
+        {
+            dtInt="E";
+           
+        }
+        else
+        {
+             dtInt="O";
+        }
+        String mnt=dt[1];
+        String yr=dt[0].substring(2);
+        String fchar="";
+        if(bookName.length()==1)
+            fchar+="x";
+        if(bookName.length()==2)
+            fchar+=bookName;
+        else if (bookName.equals(""))
+            fchar="XX";
+        else 
+            fchar=bookName.substring(0,1);
+        
+        String lchar="";
+        if(bookName.length()==1)
+            lchar+="x";
+        if(bookName.length()==2)
+            lchar+=bookName;
+        else if (bookName.equals(""))
+            lchar="XX";
+        else 
+            lchar=bookName.substring(bookName.length()-1);
+        String id=yr+"-"+mnt+"-"+dtInt+"-";
+        if (bookName.equals(""))
+            id+="XX";
+        else if (bookName.length()==2)
+            id+=bookName;
+        else 
+            id+=fchar.toUpperCase()+lchar.toUpperCase();
+         return id;
     }
 }
